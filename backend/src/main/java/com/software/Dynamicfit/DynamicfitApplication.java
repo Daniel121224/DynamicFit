24937baa094deb8de426c9ca51422d5db2757625
Daniel Robles <129451318+Daniel121224@users.noreply.github.com>
2025-04-04 09:33:16 -0500

    Add files via upload

---
+++ backend/src/main/java/com/software/Dynamicfit/DynamicfitApplication.java
@@ -0,0 +1,13 @@
+package com.software.Dynamicfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicfitApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/software/Dynamicfit/DynamicfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.Dynamicfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
